c7f38672e264f11b2d64d2020d3f2da3b15eb1a7
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-11 11:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const student = {
   name: "John",
 };
 
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
 }
 
-// Testing the function
-console.log(getKeys(student)); // Output: ["name"]
